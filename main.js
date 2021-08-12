98474e350d01a01de9c6909e068ec4487dff07b8
marcelo tito copa <matcop@gmail.com>
2021-08-12 18:19:07 -0400

    curso 17 ol instructor-manejo de multicapas part 1

---
+++ main.js
@@ -0,0 +1,128 @@
+window.onload = init;
+
+function init(){
+  // Attribution Control
+  const attributionControl = new ol.control.Attribution({
+    collapsible: true
+  })
+
+  // Map object
+  const map = new ol.Map({
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 3,     
+    }),    
+    target: 'js-map',
+    controls: ol.control.defaults({attribution: false}).extend([attributionControl])
+  })
+
+  // Base Layers
+  // Openstreet Map Standard
+  const openstreetMapStandard = new ol.layer.Tile({
+    source: new ol.source.OSM(),    
+    visible: true,
+    title: 'OSMStandard'        
+  })
+
+  // Openstreet Map Humanitarian
+  const openstreetMapHumanitarian = new ol.layer.Tile({
+    source: new ol.source.OSM({
+      url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+    }),
+    visible: false,
+    title: 'OSMHumanitarian'
+  })
+
+  // Bing Maps Basemap Layer
+  const bingMaps = new ol.layer.Tile({
+    source: new ol.source.BingMaps({
+      key: "Your Bingmaps API Key Here",
+      imagerySet: 'CanvasGray'  // Road, CanvasDark, CanvasGray
+    }),
+    visible: false,
+    title: 'BingMaps'
+  })
+
+  // CartoDB BaseMap Layer
+  const cartoDBBaseLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: 'http://{1-4}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png',
+      attributions: '© CARTO'
+    }),
+    visible: false,
+    title: 'CartoDarkAll'
+  })
+
+  // Stamen basemap layer
+  const StamenTerrainWithLabels = new ol.layer.Tile({
+    source: new ol.source.Stamen({
+      layer: 'terrain-labels',
+      attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    visible: false,
+    title: 'StamenTerrainWithLabels'
+  })
+  
+  const StamenTerrain = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
+      attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    visible: false,
+    title: 'StamenTerrain'
+  })
+
+  // Layer Group
+  const baseLayerGroup = new ol.layer.Group({
+    layers: [
+      openstreetMapStandard, openstreetMapHumanitarian, bingMaps, cartoDBBaseLayer,
+      StamenTerrainWithLabels, StamenTerrain
+    ]
+  })
+  map.addLayer(baseLayerGroup);
+
+  // Layer Switcher Logic for BaseLayers
+  const baseLayerElements = document.querySelectorAll('.sidebar > input[type=radio]')
+  for(let baseLayerElement of baseLayerElements){
+    baseLayerElement.addEventListener('change', function(){
+      let baseLayerElementValue = this.value;
+      baseLayerGroup.getLayers().forEach(function(element, index, array){
+        let baseLayerName = element.get('title');
+        element.setVisible(baseLayerName === baseLayerElementValue) 
+      })
+    })
+  }
+  
+
+  // TileDebug
+  const tileDebugLayer = new ol.layer.Tile({
+    source: new ol.source.TileDebug(),
+    visible: false
+  })
+
+  // tile ArcGIS REST API Layer
+  const tileArcGISLayer = new ol.layer.Tile({
+    source: new ol.source.TileArcGISRest({
+      url: "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Louisville/LOJIC_LandRecords_Louisville/MapServer",
+      attributions: 'Copyright© 2008, MSD, PVA, Louisville Water Company, Louisville Metro Government'
+    }),
+    visible: false
+  })
+  
+
+  // NOAA WMS Layer
+  const NOAAWMSLayer = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'https://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_dailymaxairtemp_offsets/MapServer/WMSServer?',
+      params:{
+        LAYERS: 5,
+        FORMAT: 'image/png',
+        TRANSPARENT: true
+      },
+      attributions: '<a href=https://nowcoast.noaa.gov/>© NOAA<a/>'
+    }),
+    visible: false
+  })
+}
+
+
